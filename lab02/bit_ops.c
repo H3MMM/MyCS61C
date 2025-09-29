618ad9e2be2b771971935ea4a311ec011f58401e
H3M <3435359630@qq.com>
2025-09-29 08:04:17 +0800

    完成lab2 lfsr

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,19 +9,22 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    return (x >> n) & 1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    //先把要操作的位清零
+    *x &= ~(1 << n);
+    //再讲该位设置为v
+    *x |= (v << n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    //A异或1即可得到非A
+    *x ^= (1 << n);
 }
-
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,22 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+uint16_t getBit(uint16_t *reg, int n);
+
+void lfsr_calculate(uint16_t *reg)
+{
+    // 取最低位
+    uint16_t num = getBit(reg, 0);
+    num ^= getBit(reg, 2);
+    num ^= getBit(reg, 3);
+    num ^= getBit(reg, 5);
+    // 右移一位
+    *reg >>= 1;
+    // 把运算结果放到第1位，因右移会在最高位补0，所以不用额外清零操作
+    *reg |= (num << 15);
 }
 
+uint16_t getBit(uint16_t *reg, int n)
+{
+    return (*reg >> n) & 1;
+}
