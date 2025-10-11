90dbaa69bd61174242fd37564e516553dc491fa5
H3M <3435359630@qq.com>
2025-10-11 19:53:59 +0800

    完成点积函数

--- fa20-proj2-starter/src/dot.s
+++ fa20-proj2-starter/src/dot.s
@@ -18,27 +18,45 @@
 #   this function terminates the program with error code 76.
 # =======================================================
 dot:
-
+    addi t0, x0, 1
+    blt a2, t0, lengthError
+    blt a3, t0, strideError
+    blt a4, t0, strideError
     # Prologue
-
+    mv t0, x0   #result
+    mv t1, x0   #count
 
 loop_start:
+    beq t1, a2, loop_end
+    
+    mul t2, t1, a3
+    slli t2, t2, 2  # 1 word = 4 byte 
+    add t2, a0, t2  # address of elem of v0
+    lw t2, 0(t2)
+    mul t3, t1, a4
+    slli t3, t3, 2
+    add t3, a1, t3
+    lw t3, 0(t3)
+    mul t4, t2, t3
+    add t0, t0, t4
+    
+    addi t1, t1, 1 #count++
 
-
-
-
-
-
-
-
-
-
+    j loop_start
 
 
 loop_end:
 
-
+    mv a0, t0
     # Epilogue
 
     
     ret
+
+lengthError:
+    addi a0, x0, 75
+    ecall
+
+strideError:
+    addi a0, x0, 76
+    ecall
--- fa20-proj2-starter/unittests/unittests.py
+++ fa20-proj2-starter/unittests/unittests.py
@@ -60,15 +60,20 @@ class TestArgmax(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        raise NotImplementedError("TODO")
+        #raise NotImplementedError("TODO")
         # TODO
         # load address of the array into register a0
+        array0 = t.array([1, 0, 2, 100, 3, 4])
+        t.input_array("a0", array0)
         # TODO
         # set a1 to the length of the array
+        t.input_scalar("a1", len(array0))
         # TODO
         # call the `argmax` function
+        t.call("argmax")
         # TODO
         # check that the register a0 contains the correct output
+        t.check_scalar("a0", 3)
         # TODO
         # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
         t.execute()
@@ -82,16 +87,24 @@ class TestDot(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "dot.s")
         # create arrays in the data section
-        raise NotImplementedError("TODO")
+        #raise NotImplementedError("TODO")
         # TODO
         # load array addresses into argument registers
+        array0 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        array1 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        t.input_array("a0", array0)
+        t.input_array("a1", array1)
         # TODO
         # load array attributes into argument registers
         # TODO
+        t.input_scalar("a2", 3)
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 2)
         # call the `dot` function
         t.call("dot")
         # check the return value
         # TODO
+        t.check_scalar("a0", 22)
         t.execute()
 
     @classmethod
